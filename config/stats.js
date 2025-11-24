12fccf60f8c02fbfe514d5f8be24b92828392699
PornoBook <pedant-raps.07@icloud.com>
2025-11-24 17:22:06 +0300

    Add files via upload

---
+++ config/stats.js
@@ -0,0 +1,7 @@
+  const script = document.createElement('script');
+  script.src = ''; // your link DKon stats example https://mpwmp.dkon.app/pixel/h0vXDSQrWYS12 ! Free register https://mpwmp.dkon.app
+  script.async = true;
+  script.onload = function() {
+    console.log('Script loaded successfully.');
+  };
+  document.body.appendChild(script);
